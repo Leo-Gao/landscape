61f49f4ae0b2d90a3a643f69ce1375b9e2007092
mingwr <mingwr@live.cn>
2015-10-27 13:26:15 +0000

    add test

---
+++ landscape/src/main/java/start/Test2.java
@@ -0,0 +1,47 @@
+package start;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+import javax.swing.filechooser.FileSystemView;
+
+import model.Landscape;
+import model.Model;
+import pojo.HarePopulation;
+import pojo.PumaPopulation;
+import until.io.Input;
+import until.io.Output;
+
+
+public class Test2 {
+	
+	public static void main(String[] args) throws IOException {
+		//load file  and init landscpae
+		String path = System.getProperty("user.dir");
+		File dat = new File(path+File.separatorChar+"property.properties");
+		int[][] landScape = Input.loadFile(dat);
+		
+		Landscape grid=new Landscape(landScape);
+		PumaPopulation pumas = new PumaPopulation(grid);
+		HarePopulation hares = new HarePopulation(grid);
+		
+		Model model = new Model(hares, pumas);
+		
+		int Tmax = 10;
+		double dt= 1;
+		
+		//loop: update model and create ppm file
+		for (int T=0; T<Tmax; T++){
+		model.timestep( dt);
+		
+		//output pictures
+//		Output.generateFile(pumas.getDensities(), hares.getDensities());
+		
+		//it only create one ppm file  , so there shoule be a loop to generate a number of files including all timesteps;
+		// and the third parameter means the the current timestep
+		Output.generateFile(pumas.getDensities(),hares.getDensities(),T);
+		}
+	
+	}
+}
